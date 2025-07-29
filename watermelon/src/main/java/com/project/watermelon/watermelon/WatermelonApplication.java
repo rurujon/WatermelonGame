78df25db9d20fd12efab060e1d915ed73e27d4d3
rurujon <rurujeon@naver.com>
2025-07-29 23:09:48 +0900

    기본

---
+++ watermelon/src/main/java/com/project/watermelon/watermelon/WatermelonApplication.java
@@ -0,0 +1,13 @@
+package com.project.watermelon.watermelon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatermelonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatermelonApplication.class, args);
+	}
+
+}
